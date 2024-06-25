85da4d914af10326216d537648ef2474a8579262
nitinarya1 <136402013+nitinarya1@users.noreply.github.com>
2024-06-25 18:20:52 +0530

    Add files via upload

---
+++ ecommerce website/index.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wrapco: Protection Meets Perfection"</title>
+    <!-------------------------- bootstrap CSS link --------------------------->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+     rel="stylesheet" 
+     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
+     crossorigin="anonymous">
+    <!-----------------------font awesome----------------------->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" 
+    integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+     <!-----------CSS file linking---------------->
+     <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+     <!----------------------------------------- navbar ------------------------------------------------->
+     <div class="container-fluid p-0">
+          <!-----------------------------------first child-------------------------->
+          <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <img src="./images/logo.png" alt="" class="logo">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Products</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Register</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="fa-solid fa-cart-shopping"></i><sup>1</sup></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Total price</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<!--second child welcome and login etc-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+  <ul class="navbar-nav me-auto">
+  <li class="nav-item">
+          <a class="nav-link" href="#">Welcome Guest</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Login</a>
+        </li>
+  </ul>
+
+</nav>
+<!-- third child -->
+ <div class="bg-light">
+<h3 class="text-center">Wrapco Store</h3>
+<p class="text-center">Wrapco: Your Device, Your Style, Your Statement.</p>
+
+ </div>
+
+  <!--fourth child-->
+<div class="row">
+  <div class="col-md-10">
+  <!--products-->
+  <div class="row">
+<div class="col-md-4 mb-2">
+<div class="card">
+  <img src="./images/apple/apple1.webp" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-Secondary">View More</a>
+  </div>
+</div>
+</div>
+<div class="col-md-4 mb-2">
+<div class="card">
+  <img src="./images/apple/apple2.webp" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-Secondary">View More</a>
+  </div>
+</div>
+</div>
+<div class="col-md-4 mb-2">
+<div class="card">
+  <img src="./images/apple/apple3.webp" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-Secondary">View More</a>
+  </div>
+</div>
+</div>
+<div class="col-md-4 mb-2">
+<div class="card">
+  <img src="./images/apple/apple4.webp" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-Secondary">View More</a>
+  </div>
+</div>
+</div>
+<div class="col-md-4 mb-2">
+<div class="card">
+  <img src="./images/apple/apple5.webp" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-Secondary">View More</a>
+  </div>
+</div>
+</div>
+<div class="col-md-4 mb-2">
+<div class="card">
+  <img src="./images/apple/apple6.webp" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-info">Add to cart</a>
+    <a href="#" class="btn btn-Secondary">View More</a>
+  </div>
+</div>
+</div>
+  </div>
+</div>
+  <div class="col md-2 bg-secondary p-0">
+    <!--Brands to be displayed-->
+    <ul class="navbar-nav me-auto text-center">
+
+      <li class="nav-item bg-info">
+        <a href="#" class="nav-link text-light"><h4>Delivery Brands</h4></a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link text-light">Brand1</a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link text-light">Brand2</a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link text-light">Brand3</a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link text-light">Brand4</a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link text-light">Brand5</a>
+      </li>
+    </ul>
+     <!--display category-->
+     <ul class="navbar-nav me-auto text-center">
+
+<li class="nav-item bg-info">
+  <a href="#" class="nav-link text-light"><h4>categories</h4></a>
+</li>
+<li class="nav-item">
+  <a href="#" class="nav-link text-light">Category1</a>
+</li>
+<li class="nav-item">
+  <a href="#" class="nav-link text-light">Category2</a>
+</li>
+<li class="nav-item">
+  <a href="#" class="nav-link text-light">Category3</a>
+</li>
+<li class="nav-item">
+  <a href="#" class="nav-link text-light">Category4</a>
+</li>
+<li class="nav-item">
+  <a href="#" class="nav-link text-light">Category5</a>
+</li>
+</ul>
+  <!--side nav bar--> 
+  <!--Brand to be display-->
+  </div>
+</div>
+
+
+
+
+ <!---Last child footer-->
+ <!--<div class="bg-body-tertiary p-3 text-center">
+  <p>All Rights reserved Wrapco.in - Designed by Nitin Arya-20223166 </p>
+ </div> -->
+     </div>
+
+
+
+
+
+
+
+
+    <!-------------------Bootstrap js link------------------->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
+    crossorigin="anonymous"></script>
+</body>
+</html>
